76bf62a7134c89536c9a4b7b7e5d14170c25ccf6
peggylicious <p.asq@live.com>
2026-05-07 10:34:18 +0100

    feat(65): add reference to original project site

--- apps/forms/65-signal-form-edition/src/app/app.component.ts
+++ apps/forms/65-signal-form-edition/src/app/app.component.ts
@@ -4,16 +4,27 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   imports: [RouterOutlet],
   selector: 'app-root',
+  styles: `
+    a {
+      text-decoration: #1b9df0;
+      /*color: #000;*/
+    }
+  `,
   template: `
     <div class="min-h-screen bg-gray-50 px-4 py-8 sm:px-6 lg:px-8">
       <div class="mx-auto max-w-4xl">
         <header class="mb-8 font-serif">
-          <h1 class="text-3xl font-bold leading-tight text-gray-900">
+          <h1 class="text-3xl leading-tight font-bold text-gray-900">
             User Management Portal
           </h1>
         </header>
 
         <router-outlet />
+        <footer>
+          <p>
+            <a href="">OSS contribution for Tomas Laforge Angular challenges</a>
+          </p>
+        </footer>
       </div>
     </div>
   `,
